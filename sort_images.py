8e48a1a44c76c17a65dbf3d340d0968db57dd3d6
LeoBueno15 <81589600+LeoBueno15@users.noreply.github.com>
2022-10-02 14:24:48 -0400

    sort image functions implement

--- sort_images.py
+++ sort_images.py
@@ -1,8 +1,17 @@
 from datetime import datetime
 
+
+class Image:
+    def __init__(self, image_name, file_dir, collect_date, instrument):
+        self.image_name = image_name
+        self.file_dir = file_dir
+        self.collect_date = collect_date
+        self.instrument = instrument
+
+
 def sort_date(image_list):
-    image_list.sort(key=lambda r: r.image.date)
+    return sorted(image_list, key=lambda r: datetime.strptime(r.collect_date, "%m/%d/%y"))
+
 
 def sort_object(image_list):
-
-def read_wav(wav_file):
+    return sorted(image_list, key=lambda r: r.image_name)
